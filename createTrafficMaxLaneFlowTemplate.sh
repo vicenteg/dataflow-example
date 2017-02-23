97d74064b514d27b41599c018c3a864ab5074960
Vince Gonzalez <vince.gonzalez@gmail.com>
2017-02-23 17:29:22 -0500

    added numWorkers and maxNumWorkers

--- createTrafficMaxLaneFlowTemplate.sh
+++ createTrafficMaxLaneFlowTemplate.sh
@@ -6,4 +6,4 @@ gsutil ls $stagingbucket >/dev/null || gsutil mb $stagingbucket
 
 mvn compile exec:java \
 	-Dexec.mainClass=com.example.dataflow.TrafficMaxLaneFlow \
-	-Dexec.args="--project=$project --streaming=true --pubsubTopic=projects/$project/topics/traffic-topic --inputFile='gs://dataflow-samples/traffic_sensor/Freeways-5Minaa2010-01-01_to_2010-02-15.csv' --templateLocation=$stagingbucket/TrafficMaxLaneFlow --runner=DataflowRunner"
+	-Dexec.args="--project=$project --numWorkers=1 --maxNumWorkers=4 --streaming=true --pubsubTopic=projects/$project/topics/traffic-topic --inputFile='gs://dataflow-samples/traffic_sensor/Freeways-5Minaa2010-01-01_to_2010-02-15.csv' --templateLocation=$stagingbucket/TrafficMaxLaneFlow --runner=DataflowRunner"

b5f9b7828026b55190e36d30dc59bd5ac04c33a5
Vince Gonzalez <vince.gonzalez@gmail.com>
2017-02-23 11:31:40 -0500

    fix name

--- runInjectorTemplate.sh
+++ runInjectorTemplate.sh
@@ -4,5 +4,5 @@ project=$(gcloud info | egrep ^Project | tr -d '[]' | awk '{ print $2 }')
 stagingbucket=gs://$project-staging
 
 gcloud beta dataflow jobs run pubsub-file-injector \
-	--gcs-location=$stagingbucket/PubsubInjector \
+	--gcs-location=$stagingbucket/PubsubFileInjector \
 	--parameters="outputTopic=projects/$project/topics/traffic-topic input='gs://dataflow-samples/traffic_sensor/Freeways-5Minaa2010-01-01_to_2010-02-15.csv'"
